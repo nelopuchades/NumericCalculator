f049569c2f08868b053ad97cedab8c1e591096c1
corre <corre@NELODEV-PC>
2022-01-02 21:04:04 +0100

    Add project files.

---
+++ NumericCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1;
+int number2;
+
+Console.WriteLine("Please, type the first number: ");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You've selected " + number1 + " as your first number.");
+
+Console.WriteLine("Please, type the second number: ");
+number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You've selected " + number2 + " as your second number.");
+
+int result = number1 * number2;
+Console.WriteLine("The result of " + number1 + "*" + number2 + " is " + result);
